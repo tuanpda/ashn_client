1e3dc17939fa4d3baaf289c979ed57273d6325eb
Your Name <you@example.com>
2025-06-27 19:13:40 +0700

    commit

---
+++ config.company.js
@@ -0,0 +1,13 @@
+export default {
+    companyId: "hungnguyen", // mã định danh duy nhất (dùng để switch baseURL, tên,...)
+    companyName: "CÔNG TY TNHH AN SINH HƯNG NGUYÊN",
+    companyNameThuong: "Công ty TNHH An sinh Hưng Nguyên",
+    bhxhName: "BẢO HIỂM XÃ HỘI KHU VỰC VII",
+    apiBaseURL: "http://14.224.148.17:4019", // CỔNG 4019 LÀ BACKEND CHO HƯNG NGUYÊN
+    clientURL: "http://14.224.148.17:4020", // CLIENT
+    port_client: 4020, // CỔNG CLIENT HƯNG NGUYÊN
+    port_backend: 4019,
+    host: '192.168.1.5', // MÁY CHỦ CHUNG TRÊN HỆ THỐNG PHỦ DIỄN
+    namebienlai: 'ansinhphudien',
+    urlBienlaidientu: `http://14.224.148.17:4020/tracuubienlaidientu-ansinhhungnguyen`
+};
